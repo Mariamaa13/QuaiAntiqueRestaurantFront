8c19e1585a32346228c95b85e54fa6f89eea1e53
GUIRASSY Mariama <guirassymariama.etude@gmail.com>
2026-09-03 15:09:02 +0200

    gestion de l'affichage des réservations

--- Routeur/allRoutes.js
+++ Routeur/allRoutes.js
@@ -7,7 +7,8 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
-    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html")
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html")
 
 ];
 
